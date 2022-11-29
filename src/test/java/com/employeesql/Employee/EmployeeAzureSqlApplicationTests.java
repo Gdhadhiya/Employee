5404bfb0c0c206405b0d1b6ae2663b00201ef80d
Gauri Dhadhiya <gauri.dhadhiya@nagarro.com>
2022-11-29 22:20:51 +0530

    Azure assignment code

---
+++ src/test/java/com/employeesql/Employee/EmployeeAzureSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employeesql.Employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeAzureSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
